abf0430dd66ce36a2ef3e9daf2f7e0293749b98e
Justinxtyme <binning.justin@gmail.com>
2025-04-01 15:42:05 -0700

    Update httptest.py

--- httptest.py
+++ httptest.py
@@ -1,15 +1,6 @@
 #IMPORT MINIMUM 
 from http.server import HTTPServer, BaseHTTPRequestHandler
 
-# SET HOST AND PORT
-server_address = ('localhost', 8000) 
-
-#CREATING SERVER INSTANCE
-httpd = HTTPServer(server_address, BaseHTTPRequestHandler)
-
-# START SERVER TO ALLOW CONNECT
-httpd.startforever()
-
 # DEFINING CUSTOM HANDLER
 class CustomHandler(BaseHTTPRequestHandler):
     def do_GET(self):
@@ -17,3 +8,11 @@ class CustomHandler(BaseHTTPRequestHandler):
         self.send_header('Content-type', 'text/html')  # Content type: HTML
         self.end_headers()  # End headers
         self.wfile.write(b"<html><body><h1>Hello, World!</h1></body></html>")  # Response body
+# SET HOST AND PORT
+server_address = ('localhost', 8000) 
+
+#CREATING SERVER INSTANCE
+httpd = HTTPServer(server_address, CustomHandler)
+
+# START SERVER TO ALLOW CONNECT
+httpd.startforever()
