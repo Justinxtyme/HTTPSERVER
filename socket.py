315bcf4da792990c5c001ef9ed8805c16ae87cb7
Justinxtyme <binning.justin@gmail.com>
2025-04-01 16:37:55 -0700

    Update socket.py

--- socket.py
+++ socket.py
@@ -5,8 +5,26 @@ import socket
 # CREATE TCP SOCKET AF_INET= IPV4 SOC_STREAM = TCP
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-#BINDING
+#SETTING HOST AND PORT
 server_address = ('localhost', 65432)
 #BIND SOCKET TO SERVER ADRESS
 server_socket.bind(server_address)
 print(f"Server listening on {server_address}")
+
+#OPEN FOR INCOMING CONNECTIONS
+server_socket.listen()
+
+#ACCEPTING CONNECTION
+connection, client_address = server_socket.accept()
+print(f"Connection from {client_address}")
+
+#RECIEVE DATA FROM CLIENT
+data = connection.recv(1024)  # Read up to 1024 bytes(adjustable)
+print(f"Received: {data.decode('utf-8')}")
+
+#SEND RESPONSE TO CLIENT
+connection.sendall(b"Hello, Client!") 
+
+#CLOSE IT!
+connection.close()
+
