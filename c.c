fc1020f8974e468229f16b31eecc072f3ef1a31b
Justinxtyme <binning.justin@gmail.com>
2025-08-07 18:34:48 -0700

    Update c.c

--- c.c
+++ c.c
@@ -14,3 +14,28 @@ int main() {
       printf("%i",arr[i]);
     }
 }
+
+// ***** //
+#include<stdio.h>
+#include<string.h>
+ 
+void copy(char* dst, char* src){
+  while(*src != '\0') {
+    *dst = *src;
+    *src++;
+    *dst++;
+  } *dst = '\0';
+   
+}
+ 
+int main(){
+  char srcString[] = "We promptly judged antique ivory buckles for the next prize!";
+  char dstString[strlen(srcString) + 1];
+ 
+ copy(dstString, srcString);
+ printf("%s\n", dstString);
+ printf("%s", srcString);
+ return 0;
+}
+
+
